82753797d531e3fe8949484ce4dee630729dce6d
Eric <eric.bressot-perrin@live.ca>
2020-03-31 16:31:02 -0400

    Merged source and feed into file Extracted DateSelector

--- src/FilterBar.js
+++ src/FilterBar.js
@@ -1,42 +1,18 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
+import DateSelector from './DateSelector';
 
 class FilterBar extends React.Component {
     render() {
         return (
           <Form>
-            <Form.Group controlId="source">
-              <Form.Label>Source</Form.Label>
+            <Form.Group controlId="file">
+              <Form.Label>File</Form.Label>
               <Form.Control as="select">
-                <option value="scd">Simcorp Dimension</option>
+                <option value="scd.securities">Simcorp Dimension/Securities</option>
               </Form.Control>
             </Form.Group>
-            <Form.Group controlId="feed">
-              <Form.Label>Feed</Form.Label>
-              <Form.Control as="select">
-                <option value="securities">Securities</option>
-              </Form.Control>
-            </Form.Group>
-            <Form.Row>
-              <Form.Group controlId="effectiveDateDay" className="left-column">
-                <Form.Label>Effective Date</Form.Label>
-                <Form.Control as="select">
-                  <option value="28">28</option>
-                </Form.Control>
-              </Form.Group>
-              <Form.Group controlId="effectiveDateMonth" className="middle-column">
-                <Form.Label>&nbsp;</Form.Label>
-                <Form.Control as="select">
-                  <option value="03">March</option>
-                </Form.Control>
-              </Form.Group>
-              <Form.Group controlId="effectiveDateYear" className="right-column">
-                <Form.Label>&nbsp;</Form.Label>
-                <Form.Control as="select">
-                  <option value="2020">2020</option>
-                </Form.Control>
-              </Form.Group>
-            </Form.Row>
+            <DateSelector />
           </Form>
         )
     }
