be3b499e3450b178df18ea9124f1fba2c0d19858
Eric <eric.bressot-perrin@live.ca>
2020-04-10 10:52:44 -0400

    Replaced create-react-app template Default template replaced by redux template

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/New Records/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders default page', () => {
+  const { getByText } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+
+  expect(getByText(/New Records/i)).toBeInTheDocument();
 });
